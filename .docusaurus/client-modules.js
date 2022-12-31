4eb638dcef81c3af318e0df6301fc09b79268bdf
Franck DAKIA <dakiafranck@gmail.com>
2022-12-31 13:36:29 +0000

    Remove .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/home/papac/engineering/bowphp/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/papac/engineering/bowphp/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/papac/engineering/bowphp/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/papac/engineering/bowphp/docs/src/css/customTheme.css'),
-  require('/home/papac/engineering/bowphp/docs/static/css/custom.css'),
-];
