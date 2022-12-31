d5911b07ee782e99c13d30eb0ee99094bd52e116
Franck DAKIA <dakiafranck@gmail.com>
2022-12-31 14:30:25 +0000

    Refactoring api docs

--- api-docs/api.php
+++ api-docs/api.php
@@ -5,7 +5,7 @@ use Doctum\RemoteRepository\GitHubRemoteRepository;
 use Doctum\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
-$dir = __DIR__ . '/../../framework/src';
+$dir = sys_get_temp_dir() . '/framework/src';
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
@@ -14,7 +14,7 @@ $iterator = Finder::create()
     ->in($dir);
 
 $versions = GitVersionCollection::create($dir)
-    ->addFromTags('v4.3.*')
+    ->addFromTags('v4.4.*')
     ->add('4.0', '4.0 branch')
     ->add("master", "Dev Branch");
 
@@ -22,8 +22,8 @@ return new Doctum($iterator, [
     'versions'             => "master",
     'title'                => 'Bow Framework',
     'language'             => 'en', // Could be 'fr'
-    'build_dir'            => __DIR__ . '/build/%version%',
-    'cache_dir'            => __DIR__ . '/cache/%version%',
+    'build_dir'            => __DIR__ . '/../build/api/%version%',
+    'cache_dir'            => __DIR__ . '/../cache/api/%version%',
     'source_dir'           => dirname($dir) . '/',
     'remote_repository'    => new GitHubRemoteRepository('bowphp/framework', dirname($dir)),
     'default_opened_level' => 2,
