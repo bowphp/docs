bd38bed1ca2eb458ecee1c36054e4a315d9969c7
Franck DAKIA <dakiafranck@gmail.com>
2023-01-07 22:05:15 +0000

    Fixes php codeblock highlight

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,29 +7,52 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import SponsorsFeatures from '@site/src/components/SponsorsFeatures';
 import styles from './index.module.css';
 import TeamFeatures from '../components/TeamFeatures';
+import CodeBlock from "@theme/CodeBlock";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <header className={clsx('', styles.heroBanner)}>
+    <header className={clsx("", styles.heroBanner)}>
       <div className="container">
         <div className="row">
           <div className="col">
             <h1 className="hero__title">{siteConfig.title}</h1>
             <p className="hero__subtitle">{siteConfig.tagline}</p>
-            <p 
+            <p
               className="hero__subtitle"
-              style={{fontSize: 15, borderLeft: "5px solid red", display: "inline-block", paddingLeft: 3}}
-              dangerouslySetInnerHTML={{__html: siteConfig.customFields.landingText}}
+              style={{
+                fontSize: 15,
+                borderLeft: "10px solid #452c2c",
+                display: "inline-block",
+                paddingLeft: 10,
+              }}
+              dangerouslySetInnerHTML={{
+                __html: siteConfig.customFields.landingText,
+              }}
             ></p>
             <div className={styles.buttons}>
               <Link
                 className="button button--secondary button--lg"
-                to={siteConfig.customFields.startTutorial}>
+                to={siteConfig.customFields.startTutorial}
+              >
                 Tutoriel - 5min ⏱️
               </Link>
             </div>
+          </div>
+          <div className="col" style={{ textAlign: "left" }}>
+            <CodeBlock
+              language="php"
+              title="routes/app.php"
+              showLineNumbers
+            >
+              {`<?php
+use App\Controllers\WelcomeController;
+
+$app->get('/', WelcomeController::class)->name('app.index');`}
+            </CodeBlock>
           </div>
         </div>
       </div>
