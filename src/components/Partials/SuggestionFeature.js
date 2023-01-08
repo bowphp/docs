b98233a58d6465ef7fb32a9a1d5700973297cab2
Franck DAKIA <dakiafranck@gmail.com>
2023-01-08 21:14:05 +0000

    Formatting

--- src/components/Partials/SuggestionFeature.js
+++ src/components/Partials/SuggestionFeature.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-const SuggestionFeature = () => 
+const SuggestionFeature = () => (
   <>
     <h2>Il manque quelque chose ?</h2>
     <p>
       Si vous rencontrez des problèmes avec la documentation ou si vous avez des
       suggestions pour améliorer la documentation ou le projet en général,
       veuillez déposer une issue pour nous, ou envoyer un tweet mentionnant le
-      compte Twitter [@bowframework](https://twitter.com/@bowframework) ou sur
-      directement sur le [github](https://github.com/bowphp/docs/issues).
+      compte Twitter{" "}
+      <a href="https://twitter.com/@bowframework">@bowframework</a> ou sur
+      directement sur le{" "}
+      <a href="https://github.com/bowphp/docs/issues">github</a>.
     </p>
-  </>;
+  </>
+);
 
 export default SuggestionFeature;
