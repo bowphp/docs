58bf95c8b1dda1b54e83ccc3ff0e1cc7ba9e9f7d
Franck DAKIA <dakiafranck@gmail.com>
2025-01-14 03:48:18 +0000

    refactor: refont api-doc buider and refactoring existance docs

--- api-docs/build.sh
+++ api-docs/build.sh
@@ -1,5 +1,5 @@
 # Ge the bow framework source
-[ -d /tmp/framework ] || git clone https://github.com/bowphp/framework /tmp/framework
+[ -d ./framework ] || git clone https://github.com/bowphp/framework ./framework
 
 # Install the doctum if not exists
 [ -f ./doctum.phar ] || curl -O https://doctum.long-term.support/releases/latest/doctum.phar
---
+++ api-docs/readme.md
@@ -0,0 +1,9 @@
+## Api docs buider
+
+Lance the following commande:
+
+Git is required
+
+```
+bash ./build.sh
+```
